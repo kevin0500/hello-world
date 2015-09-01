0aedcaaa37283728b3dfae81ac71ae0cd1fae6f3
unknown <min04.wang@PC-150400076.freecomm-networks.com>
2015-09-01 11:29:00 +0800

    test1

---
+++ test/src/a/Test01.java
@@ -0,0 +1,25 @@
+package a;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Test01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String pattern = "yyyy-MM-dd HH:mm:ss";
+		System.out.println(format(new Date(), pattern));
+		
+//		System.getenv().get("Username");
+		System.getProperty("catalina.home");
+		
+	}
+	
+	public static String format(Date date, String pattern) {
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        return sdf.format(date);
+    } 
+}
